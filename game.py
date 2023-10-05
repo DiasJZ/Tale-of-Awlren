2c52d03505143761e0c12844f4cafb3553645ded
João Vitor Dias <joaovdias55@gmail.com>
2023-10-04 22:33:21 -0300

    .

--- game.py
+++ game.py
@@ -13,11 +13,20 @@ PRETO = (0,0,0)
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Tale of Awlren')
 
-class Elfo(pygame.sprite.Sprite):
+class Sapo(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
-        self.sprites.append(pygame.image.load(''))
+        self.sprites.append(pygame.image.load('Elf\elf_regular_hair.png'))
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 100, 100
+
+todas_as_sprites = pygame.sprite.Group()
+sapo = Sapo()
+todas_as_sprites.add(elfo)
 
 while True:
     tela.fill(PRETO)
@@ -26,6 +35,8 @@ while True:
             pygame.quit()
             exit()
 
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update
     pygame.display.flip()
 
 
