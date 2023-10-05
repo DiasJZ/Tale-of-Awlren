fc89b187408ea7babb24f470399b5e15795dd806
João Vitor Dias <joaovdias55@gmail.com>
2023-10-04 22:55:43 -0300

    Inserindo Sprites

--- game.py
+++ game.py
@@ -17,12 +17,29 @@ class Sapo(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
-        self.sprites.append(pygame.image.load('Elf\elf_regular_hair.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_1.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_2.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_3.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_4.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_5.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_6.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_7.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_8.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_9.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_10.png'))
         self.atual = 0
         self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3))
 
         self.rect = self.image.get_rect()
         self.rect.topleft = 100, 100
+
+    def update(self):
+        self.atual = self.atual + 1
+        if self.atual >= len(self.sprites):
+            self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3))
 
 todas_as_sprites = pygame.sprite.Group()
 sapo = Sapo()
